b130dbe0068519057578a41a04bb86fb48f469a2
dondi <dondi@egr-qp0121965ru.lmumain.edu>
2010-10-19 09:35:07 -0700

    Initial versions of temperature examples.

---
+++ temperature-dom/temperature-dom.js
@@ -0,0 +1,14 @@
+var report = function(celsius, fahrenheit) {
+    document.getElementById("result").innerHTML = celsius + "\xb0C = "
+            + fahrenheit + "\xb0F";
+};
+
+document.getElementById("f_to_c").onclick = function() {
+    var f = document.getElementById("temperature").value;
+    report((f - 32) / 1.8, f);
+};
+
+document.getElementById("c_to_f").onclick = function() {
+    var c = document.getElementById("temperature").value;
+    report(c, 1.8 * c + 32);
+};
