089d18340c4f603dd312581e9ac5aa6c1317a94b
dondi <dondi@egr-qp0121965ru.lmumain.edu>
2010-10-28 10:58:45 -0700

    Style tweaks, plus initial event handling.

--- ipad-keyboard/ipad-keyboard.js
+++ ipad-keyboard/ipad-keyboard.js
@@ -1,3 +1,15 @@
 $(function () {
+    // Initialize all keys as inactive.
+    $("div.key span").addClass("inactive");
 
+    // Set up the event handlers.
+    $("div.key span").mousedown(function (event) {
+        $(event.currentTarget).removeClass("inactive");
+        $(event.currentTarget).addClass("active");
+    })
+
+    .mouseup(function (event) {
+        $(event.currentTarget).removeClass("active");
+        $(event.currentTarget).addClass("inactive");
+    });
 });
