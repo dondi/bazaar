c95bf5d7ecc85361f49e0faad0758a51bb3d4608
Matt040387 <Matt040387@192.168.1.103>
2010-11-04 00:17:02 -0700

    Integrated Matt + Ian + Forney + Marc's (?) lexical distance and visual feedback changes.
    Signed-off-by: dondi <dondi@10.0.1.209>

--- ipad-keyboard/ipad-keyboard.js
+++ ipad-keyboard/ipad-keyboard.js
@@ -11,5 +11,57 @@ $(function () {
     .mouseup(function (event) {
         $(event.currentTarget).removeClass("active");
         $(event.currentTarget).addClass("inactive");
+        
+        var typedText     = $('.typed-text'),
+            textToType    = $('.text-to-type'),
+            textToTypeVal = textToType.text();
+        
+        // Append one character to typed area
+        typedText.append(textToTypeVal.charAt(0));
+        
+        // Remove character from area to type
+        textToType.text((textToTypeVal.slice(1)));
     });
 });
+    
+/**
+ * Performs the lexical distance analysis from the target string to the
+ * user's input, then returns the number of differences (errors) as an
+ * integer Derived from Levenshtein Distance article on Wikipedia :D
+ */
+var errors = function (targetString, userString) {
+    var i, j, cost,
+    d = [];
+    
+    if (targetString.length === 0) {
+        return userString.length;
+    }
+    if (userString.length === 0) {
+        return targetString.length;
+    }
+    
+    for (i = 0; i <= targetString.length; i += 1) {
+        d[i] = [];
+        d[i][0] = i;
+    }
+    
+    for (j = 0; j <= userString.length; j += 1) {
+        d[0][j] = j;
+    }
+    
+    for (i = 1; i <= targetString.length; i += 1) {
+        for (j = 1; j <= userString.length; j += 1) {
+            if (targetString.charAt(i - 1) === userString.charAt(j - 1)) {
+                cost = 0;
+            } else {
+                cost = 1;
+            }
+            d[i][j] = Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost);
+            if (i > 1 && j > 1 && targetString.charAt(i - 1) === userString.charAt(j - 2) && targetString.charAt(i - 2) === userString.charAt(j - 1)) {
+                d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
+            }
+        }
+    }
+
+    return d[targetString.length][userString.length];
+};
