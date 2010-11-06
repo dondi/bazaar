bd41aa6455cf468ef8dccabed605e1ff5662c68f
dondi <dondi@10.0.1.209>
2010-11-06 11:35:23 -0700

    On-going tweaks toward the final iPad keyboard test harness.

--- ipad-keyboard/ipad-keyboard-keys.js
+++ ipad-keyboard/ipad-keyboard-keys.js
@@ -1,14 +1,34 @@
 /**
  * Created by: Brian Handy & Andrew Garrard
  */
-
-var keysArray = [ 8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094,
-        6.966, 0.153, 0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987,
-        6.327, 9.056, 2.758, 0.978, 2.360, 0.150, 1.974, 0.074 ];
-
-var lettersArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-        "z" ];
+var letterSizes = {
+    "a": 8.167,
+    "b": 1.492,
+    "c": 2.782,
+    "d": 4.253,
+    "e": 12.702,
+    "f": 2.228,
+    "g": 2.015,
+    "h": 6.094,
+    "i": 6.966,
+    "j": 0.153,
+    "k": 0.772,
+    "l": 4.025,
+    "m": 2.406,
+    "n": 6.749,
+    "o": 7.507,
+    "p": 1.929,
+    "q": 0.095,
+    "r": 5.987,
+    "s": 6.327,
+    "t": 9.056,
+    "u": 2.758,
+    "v": 0.978,
+    "w": 2.360,
+    "x": 0.150,
+    "y": 1.974,
+    "z": 0.074
+};
 
 var enhancedKeys = true;
 var minWidth = 77;
@@ -17,19 +37,19 @@ var normalWidth = 79;
 var normalHeight = 50;
 var maxWidth = 90;
 var maxHeight = 61;
-var maxUsage = keysArray[4];
+var maxUsage = letterSizes["e"];
 var spanMargin = 2;
 
 // Function for making keys the right size.
-var changeSize = function (id, slot) {
+var changeSize = function (id, size) {
     // Change the key size
     if (enhancedKeys) {
         $(id).css({
             "width" : ""
-                    + (minWidth + ((maxWidth - minWidth) * (keysArray[slot] / maxUsage)))
+                    + (minWidth + ((maxWidth - minWidth) * (size / maxUsage)))
                     + "px",
             "height" : ""
-                    + (minHeight + ((maxHeight - minHeight) * (keysArray[slot] / maxUsage)))
+                    + (minHeight + ((maxHeight - minHeight) * (size / maxUsage)))
                     + "px"
         });
 
@@ -38,22 +58,22 @@ var changeSize = function (id, slot) {
         $(id).css("margin-top",
             ""
             + spanMargin
-            - ((maxHeight - minHeight) * (keysArray[slot] / maxUsage))
+            - ((maxHeight - minHeight) * (size / maxUsage))
             / 2 + "px");
         $(id).css("margin-bottom",
             ""
             + spanMargin
-            - ((maxHeight - minHeight) * (keysArray[slot] / maxUsage))
+            - ((maxHeight - minHeight) * (size / maxUsage))
             / 2 + "px");
         $(id).css("margin-left",
             ""
             + spanMargin
-            - ((maxWidth - minWidth) * (keysArray[slot] / maxUsage))
+            - ((maxWidth - minWidth) * (size / maxUsage))
             / 2 + "px");
         $(id).css("margin-right",
             ""
             + spanMargin
-            - ((maxWidth - minWidth) * (keysArray[slot] / maxUsage))
+            - ((maxWidth - minWidth) * (size / maxUsage))
             / 2 + "px");
     } else {
         // Or reset key size
@@ -66,7 +86,7 @@ var changeSize = function (id, slot) {
 
 // Loop to update all keys
 $(function() {
-    for (i = 0; i < keysArray.length; i++) {
-        changeSize("#" + lettersArray[i] + "-key", i);
-    }
+    $.each(letterSizes, function (index, value) {
+        changeSize("#" + index + "-key", value);
+    });
 });
