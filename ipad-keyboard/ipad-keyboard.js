3e55947f481f3f89c692fb85d84380800f50f337
dondi <dondi@10.0.1.229>
2010-11-08 00:50:58 -0800

    Added more strings to type and ran JSLint on ipad-keyboard.js.

--- ipad-keyboard/ipad-keyboard.js
+++ ipad-keyboard/ipad-keyboard.js
@@ -3,15 +3,29 @@ $(function () {
     var stringLibrary = [
         "the quick brown fox jumps over the lazy dog",
         "seventy six trombones led the big parade",
-        "she sells seashells by the seashore"
-    ];
-
-    // Initialize all keys as inactive.
-    $("div.key").addClass("inactive");
+        "four score and seven years ago",
+        "ask not what this country can do for you",
+        "we the people of the united states",
+        "when in the course of human events",
+        "mary mary quite contrary, how does your garden grow",
+        "to be or not to be, that is the question",
+        "friends, romans, countrymen, lend me your ears",
+        "it was the best of times, it was the worst of times",
+        "criminals are a superstitious and cowardly lot",
+        "romeo, romeo, wherefore art thou romeo",
+        "he has eaten me out of house and home",
+        "play it once, sam, for old times sake",
+        "no, mr. bond, i want you to die",
+        "nobody thinks in terms of human beings",
+        "everybody ought to go careful in a city like this",
+        "i have the advantage of knowing your habits",
+        "in his eyes she eclipses and predominates",
+        "science is the belief in the ignorance of experts"
+    ],
 
     // Experimental keyboard setup function: we will call this when the
     // user requests for the experimental keyboard.
-    var setupExperimentalKeyboard = function () {
+    setupExperimentalKeyboard = function () {
         /**
          * Created by: Brian Handy & Andrew Garrard
          */
@@ -51,60 +65,78 @@ $(function () {
             normalHeight = 41,
             maxWidth = 70,
             maxHeight = 52,
-            maxUsage = letterSizes["e"],
+            maxUsage = letterSizes.e,
             spanMargin = 1,
-            spanPadding = 18;
+            spanPadding = 18,
 
-        // Function for making keys the right size.
-        var changeSize = function (id, size) {
-            // Change the key size.
-            if (enhancedKeys) {
-                var diffWidth = (maxWidth - minWidth) * (size / maxUsage),
-                    diffHeight = (maxHeight - minHeight) * (size / maxUsage),
-                    marginWidth = Math.floor(spanMargin - (diffWidth / 2)),
-                    marginHeight = Math.floor(spanMargin - (diffHeight / 2));
-
-                $(id).css({
-                    "width" : (minWidth + diffWidth) + "px",
-                    "height" : (minHeight + diffHeight) + "px"
-                })
-
-                // Change the margin size to keep the hit area centered.
-                .css("margin-top", marginHeight + "px")
-                .css("margin-bottom", marginHeight + "px")
-                .css("margin-left", marginWidth + "px")
-                .css("margin-right", marginWidth + "px")
-                
-                // Adjust the padding to compensate for this shift.
-                .css("padding-top", (spanPadding - marginHeight) + "px");
-            } else {
-                // Or reset key size
-                $(id).css({
-                    "width" : normalWidth + "px",
-                    "height" : normalHeight + "px"
-                });
-            }
-        };
+            // Function for making keys the right size.
+            changeSize = function (id, size) {
+                // Change the key size.
+                if (enhancedKeys) {
+                    var diffWidth = (maxWidth - minWidth) * (size / maxUsage),
+                        diffHeight = (maxHeight - minHeight) * (size / maxUsage),
+                        marginWidth = Math.floor(spanMargin - (diffWidth / 2)),
+                        marginHeight = Math.floor(spanMargin - (diffHeight / 2));
+    
+                    $(id).css({
+                        "width" : (minWidth + diffWidth) + "px",
+                        "height" : (minHeight + diffHeight) + "px"
+                    })
+    
+                    // Change the margin size to keep the hit area centered.
+                    .css("margin-top", marginHeight + "px")
+                    .css("margin-bottom", marginHeight + "px")
+                    .css("margin-left", marginWidth + "px")
+                    .css("margin-right", marginWidth + "px")
+                    
+                    // Adjust the padding to compensate for this shift.
+                    .css("padding-top", (spanPadding - marginHeight) + "px");
+                } else {
+                    // Or reset key size
+                    $(id).css({
+                        "width" : normalWidth + "px",
+                        "height" : normalHeight + "px"
+                    });
+                }
+            };
 
         $.each(letterSizes, function (index, value) {
             changeSize("#" + index + "-key-hit", value);
         });
-    };
+    },
     
     // State variables ("model").
-    var ticking,
-        hr_start,
-        min_start,
-        sec_start,
-        hr_end,
-        min_start,
-        sec_start,
-        targetText;
+    ticking,
+    hr_start,
+    min_start,
+    sec_start,
+    hr_end,
+    min_end,
+    sec_end,
+    targetText,
+
+
+    // The function for "advancing" the character one at a time.
+    advanceText = function () {
+        var typedText = $('.typed-text'),
+            currentText = $('.current-text'),
+            textToType = $('.text-to-type'),
+            textToTypeVal = textToType.text();
+        
+        // Append the current text to the typed area.
+        typedText.text(typedText.text() + currentText.text());
+        
+        // Set the new current text.
+        currentText.text(textToTypeVal.charAt(0));
+        
+        // Remove character from area to type.
+        textToType.text((textToTypeVal.slice(1)));
+    },
 
     // Timing run setup function: reset things so that the web page is
     // ready to accept "keypresses," track progress, and time the whole
     // thing.
-    var startTimingSession = function () {
+    startTimingSession = function () {
         // Choose a string at random and stick it in text-to-type; blank
         // out the other spans.
         targetText = stringLibrary[Math.floor((Math.random() *
@@ -121,51 +153,34 @@ $(function () {
 
         // All done; move to the first character.
         advanceText();
-    };
-
-    // The function for "advancing" the character one at a time.
-    var advanceText = function () {
-        var typedText = $('.typed-text'),
-            currentText = $('.current-text'),
-            textToType = $('.text-to-type'),
-            textToTypeVal = textToType.text();
-        
-        // Append the current text to the typed area.
-        typedText.text(typedText.text() + currentText.text());
-        
-        // Set the new current text.
-        currentText.text(textToTypeVal.charAt(0));
-        
-        // Remove character from area to type.
-        textToType.text((textToTypeVal.slice(1)));
-    };
+    },
     
     // The function for ending the timing session.
-    var endTimingSession = function () {
-        var end = new Date();
+    endTimingSession = function () {
+        var end = new Date(), minute, sec;
         hr_end = end.getHours();
         min_end = end.getMinutes();
         sec_end = end.getSeconds();
 
-        var minute;
-        var sec;
         if (hr_end !== hr_start) {
             minute = 60 - min_start + min_end;
         } else {
             minute = min_end - min_start;
         }
+
         if (min_end !== min_start) {
             sec = 60 - sec_start + sec_end;
             minute = minute - 1;
         } else {
             sec = sec_end - sec_start;
         }
+
         $("#timing-result").text(minute + ":" + sec)
             .fadeIn();
-    };
+    },
 
     // Visual feedback function for errors.
-    var flashError = function () {
+    flashError = function () {
         $("#content-to-type").removeClass("content-normal")
             .addClass("content-error");
         
@@ -173,8 +188,11 @@ $(function () {
         setTimeout(function () {
             $("#content-to-type").removeClass("content-error")
                 .addClass("content-normal");
-        }, 100);
+        }, 200);
     };
+
+    // Initialize all keys as inactive.
+    $("div.key").addClass("inactive");
 
     // Set up the event handlers.
     $("div.key span").mousedown(function (event) {
@@ -193,8 +211,11 @@ $(function () {
     })
 
     .mouseup(function (event) {
-        $(event.currentTarget.parentNode).removeClass("active")
-            .addClass("inactive");
+        // Allow a little delay before the highlight goes away.
+        setTimeout(function () {
+            $(event.currentTarget.parentNode).removeClass("active")
+                .addClass("inactive");
+        }, 200);
         
         // Is the hit "key" correct?
         if (($(event.currentTarget).text().toLowerCase() ===
