7da9aad081b320f4eb054e2d4e526af8f0e04242
dondi <dondi@egr-qp0121965ru.lmumain.edu>
2010-11-15 18:13:52 -0800

    Enabled multitouch/multimove behavior.

--- boxes-touch/boxes-touch.js
+++ boxes-touch/boxes-touch.js
@@ -24,16 +24,16 @@ var BoxesTouch = {
      * Tracks a box as it is rubberbanded or moved across the drawing area.
      */
     trackDrag: function (event) {
-        // Don't bother if we aren't tracking anything.
-        if (event.currentTarget.movingBox) {
-            $.each(event.touches, function (index, touch) {
+        $.each(event.changedTouches, function (index, touch) {
+            // Don't bother if we aren't tracking anything.
+            if (touch.target.movingBox) {
                 // Reposition the object.
-                event.currentTarget.movingBox.offset({
-                    left: touch.pageX - event.currentTarget.deltaX,
-                    top: touch.pageY - event.currentTarget.deltaY
+                touch.target.movingBox.offset({
+                    left: touch.pageX - touch.target.deltaX,
+                    top: touch.pageY - touch.target.deltaY
                 });
-            });
-        }
+            }
+        });
         
         // Don't do any touch scrolling.
         event.preventDefault();
@@ -43,17 +43,13 @@ var BoxesTouch = {
      * Concludes a drawing or moving sequence.
      */
     endDrag: function (event) {
-        if (event.currentTarget.movingBox) {
-            // Perform the actual move.
-            this.moveTarget
-                .width(this.movingBox.width())
-                .height(this.movingBox.height())
-                .offset(this.movingBox.offset());
-            
-            // Remove the temporary box from the page and drawing area.
-            this.movingBox.remove();
-            this.movingBox = null;
-        }
+        $.each(event.changedTouches, function (index, touch) {
+            if (touch.target.movingBox) {
+                // Change state to "not-moving-anything" by clearing out
+                // touch.target.movingBox.
+                touch.target.movingBox = null;
+            }
+        });
     },
 
     /**
@@ -67,34 +63,19 @@ var BoxesTouch = {
      * Begins a box move sequence.
      */
     startMove: function (event) {
-        $.each(event.touches, function (index, touch) {
+        $.each(event.changedTouches, function (index, touch) {
             // Highlight the element.
             $(touch.target).addClass("box-highlight");
 
             // Take note of the box's current (global) location.
             var jThis = $(touch.target),
-                startOffset = jThis.offset(),
-
-                // Grab the drawing area (this element's parent).
-                // We want the actual element, and not the jQuery wrapper
-                // that usually comes with it.
-                parent = jThis.parent().get(0),
-
-                // Feedback: create a temporary box during the move.
-                // This is the actual box that moves, until the end.
-                movingBox = $("<div></div>")
-                    .appendTo(parent)
-                    .addClass("box-transient")
-                    .width(jThis.width())
-                    .height(jThis.height())
-                    .offset(jThis.offset());
+                startOffset = jThis.offset();
 
             // Set the drawing area's state to indicate that it is
             // in the middle of a move.
-            parent.moveTarget = jThis;
-            parent.deltaX = touch.pageX - startOffset.left;
-            parent.deltaY = touch.pageY - startOffset.top;
-            parent.movingBox = movingBox;
+            touch.target.movingBox = jThis;
+            touch.target.deltaX = touch.pageX - startOffset.left;
+            touch.target.deltaY = touch.pageY - startOffset.top;
         });
 
         // Eat up the event so that the drawing area does not
