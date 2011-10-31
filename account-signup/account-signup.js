030ab93ef10e818c065f1d8b692659070789bc78
dondi <dondi@lmu.edu>
2011-10-31 16:37:04 -0700

    A few more account signup tweaks.

--- account-signup/account-signup.js
+++ account-signup/account-signup.js
@@ -4,9 +4,11 @@
  * Prerequisite: Load jQuery prior to loading this script.
  */
 $(function () {
-    /* We will be enabling and disabling this form as the
-       user types things in.  For convenience, we put logic
-       that we will use a lot in their own functions. */
+    /*
+     * We will be enabling and disabling this form as the
+     * user types things in.  For convenience, we put logic
+     * that we will use a lot inside their own functions.
+     */
     var updatePasswordMatchMessages = function () {
         // We leave visibility to the validation code.
         $("#password-match").css({ opacity: 0 });
@@ -70,29 +72,30 @@ $(function () {
         }
     }
 
-    /* HTML5 alert!  "input" is so new jQuery does not have it yet.
-       The bind function is the "backdoor" for things like that.
-       Note how, with everything placed in validateSignup, we can
-       assign handlers in one fell swoop! */
+    /* 
+     * HTML5 alert!  "input" is so new jQuery does not have it yet.
+     * The bind function is the "backdoor" for things like that.
+     * Note how, with everything placed in validateSignup, we can
+     * assign handlers in one fell swoop!
+     */
     $("#username, #password, #password-repeat").bind("input", validateSignup);
 
     $("#cancel").click(function () {
         if (confirm("Are you sure you want to cancel signup?")) {
-            window.location = "http://www.google.com/";
+            window.location = "http://www.google.com";
         }
     });
 
     $("#submit").click(function () {
         // If validateSignup does its job, we can go right into
         // assuming that everything is OK when we get here.
-        var input = "<p>Name: " + $("#username").val() + "<br />" + "Gender: " + $("input[@name=sex]:checked").val() + "<br />" + "</p>";
-
-        $('body').append(input);
-
-        if ($('input[type=checkbox]:checked').val() === undefined) {
-            $('body').append("<p>" + "You have chosen not to receive emails." + "</p>");
-        } else {
-            $('body').append("<p>" + "You have chosen to receive emails." + "</p>");
-        }
+        $("body").append($("<p>Name: " + $("#username").val() + "</p>" +
+                "Gender: " + $("input[name=sex]:checked").val() + "</p>" +
+                ($("input[type=checkbox]:checked").val() ?
+                        "<p>" + "You have chosen to receive emails." + "</p>" :
+                        "<p>" + "You have chosen not to receive emails." + "</p>")));
     });
+    
+    // Call validateSignup once just to make sure everything is consistent.
+    validateSignup();
 });
