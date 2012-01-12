02454dd8c21653c88c8f20f4eb25ff64af6ff674
dondi <dondi@lmu.edu>
2012-01-12 09:10:30 -0800

    Laid out canvas starting point.

---
+++ canvas-template/canvas-template.js
@@ -0,0 +1,25 @@
+/*
+ * This template file is meant to be a template for canvas-based
+ * web page code.  Nothing here is set in stone; it is mainly
+ * intended to save you some typing.
+ */
+// Yes, we can use jQuery here, but avoid it just in case you
+// really don't want to use it.  We do still keep things away
+// from the global namespace.
+(function () {
+    // Ditto on using jQuery here.
+    var canvas = document.getElementById("canvas"),
+        renderingContext = canvas.getContext("2d"),
+
+        // Declare other variables here.
+        radialGradient = renderingContext.createRadialGradient(160, 160, 1, 180, 180, 320);
+
+    // Put your canvas drawing code (and any other code) here.
+    radialGradient.addColorStop(0, "white");
+    radialGradient.addColorStop(1, "blue");
+
+    renderingContext.fillStyle = radialGradient;
+    renderingContext.beginPath();
+    renderingContext.arc(256, 256, 200, 0, Math.PI * 2, true);
+    renderingContext.fill();
+}());
