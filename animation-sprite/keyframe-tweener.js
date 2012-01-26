aadbcf807b93c3c9cc45868477a0c4f302226ed9
dondi <dondi@lmu.edu>
2012-01-25 23:36:10 -0800

    Initial layout for keyframe-tweening module and harness.

---
+++ animation-sprite/keyframe-tweener.js
@@ -0,0 +1,69 @@
+/*
+ * A simple keyframe-tweening animation module for 2D
+ * canvas elements.
+ */
+var KeyframeTweener = {
+    // The module comes with a library of common easing functions.
+    linear: function (currentTime, start, distance, duration) {
+        var percentComplete = currentTime / duration;
+        return distance * percentComplete + start;
+    },
+
+    quadEaseIn: function (currentTime, start, distance, duration) {
+        var percentComplete = currentTime / duration;
+        return distance * percentComplete * percentComplete + start;
+    },
+
+    quadEaseOut = function (currentTime, start, distance, duration) {
+        var percentComplete = currentTime / duration;
+        return -distance * percentComplete * (percentComplete - 2) + start;
+    },
+
+    quadEaseInAndOut = function (currentTime, start, distance, duration) {
+        var percentComplete = currentTime / (duration / 2);
+        return (percentComplete < 1) ?
+            (distance / 2) * percentComplete * percentComplete + start :
+            (-distance / 2) * ((percentComplete - 1) * (percentComplete - 3) - 1) + start;
+    },
+
+    // The big one: animation initialization.  The settings parameter
+    // is expected to be a JavaScript object with the following
+    // properties:
+    //
+    // - renderingContext: the 2D canvas rendering context to use
+    // - sprites: the array of sprites to animate
+    // - frameRate: number of frames per second (default 24)
+    //
+    // In turn, each sprite is a JavaScript object with the following
+    // properties:
+    //
+    // - draw: the function that draws the sprite
+    // - keyframes: the array of keyframes that the sprite should follow
+    //
+    // Finally, each keyframe is a JavaScript object with the following
+    // properties.  Unlike the other objects, defaults are provided in
+    // case a property is not present:
+    //
+    // - frame: the global animation frame number in which this keyframe
+    //          it to appear
+    // - ease: the easing function to use (default is KeyframeTweener.linear)
+    // - tx, ty: the location of the sprite (default is 0, 0)
+    // - sx, sy: the scale factor of the sprite (default is 1, 1)
+    // - rotate: the rotation angle of the sprite (default is 0)
+    //
+    // Initialization primarily calls setInterval on a custom-built
+    // frame-drawing (and updating) function.
+    initialize: function (settings) {
+        // We need to keep track of the current frame.
+        var currentFrame = 0,
+            sprites = settings.sprites;
+
+        setInterval(function () {
+            // For every sprite, determine where we are in the animation,
+            // calculate the correct transform, then draw the sprite.
+
+            // Move to the next frame.
+            currentFrame += 1;
+        }, 1000 / (settings.frameRate || 24));
+    }
+};
---
+++ animation-sprite/keyframe-tweening-demo.js
@@ -0,0 +1,84 @@
+/*
+ * This file demonstrates how our homebrew keyframe-tweening
+ * engine is used.
+ */
+(function () {
+    var
+
+        // First, a selection of "drawing functions" from which we
+        // can choose.  Their common trait: they all accept a single
+        // renderingContext argument.
+        square = function (renderingContext) {
+            renderingContext.fillRect(-20, -20, 40, 40);
+        },
+
+        circle = function (renderingContext) {
+            renderingContext.beginPath();
+            renderingContext.arc(0, 0, 50, 0, Math.PI * 2);
+            renderingContext.stroke();
+        },
+
+        // Then, we have "easing functions" that determine how
+        // intermediate frames are computed.
+
+        // Now, to actually define the animated sprites.  Each sprite
+        // has a drawing function and an array of keyframes.
+        sprites = [
+            {
+                draw: square,
+                keyframes: [
+                    {
+                        frame: 0,
+                        tx: 5,
+                        ty: 5,
+                        ease: KeyframeTweener.linear
+                    },
+
+                    {
+                        frame: 30,
+                        tx: 100,
+                        ty: 50,
+                        ease: KeyframeTweener.quadEaseIn
+                    },
+
+                    // The last keyframe does not need an easing function.
+                    {
+                        frame: 80,
+                        tx: 80,
+                        ty: 500,
+                        rotate: 60
+                    }
+                ]
+            },
+
+            {
+                draw: circle,
+                keyframes: [
+                    {
+                        frame: 50,
+                        tx: 300,
+                        ty: 200,
+                        sx: 0.5,
+                        sy: 0.5,
+                        ease: KeyframeTweener.quadEaseOut
+                    },
+
+                    {
+                        frame: 100,
+                        tx: 300,
+                        ty: 0,
+                        sx: 3,
+                        sy: 0.1
+                    }
+                ]
+            }
+        ];
+
+    // Finally, we initialize the engine.  Mainly, it needs
+    // to know the rendering context to use.  And the animations
+    // to display, of course.
+    KeyframeTweener.initialize({
+        renderingContext: document.getElementById("canvas").getContext("2d"),
+        sprites: sprites
+    });
+}());
