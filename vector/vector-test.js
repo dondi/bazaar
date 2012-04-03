51bdd096c360c0c9577af94ceaa668f0e49fd58f
dondi <dondi@lmu.edu>
2012-04-02 17:26:48 -0700

    Initial setup for Vector module.

---
+++ vector/vector-test.js
@@ -0,0 +1,24 @@
+/*
+ * Unit tests for our vector object.
+ */
+$(function () {
+
+    test("Addition and Subtraction", function () {
+    });
+
+    test("Scalar Multiplication and Division", function () {
+    });
+
+    test("Dot Product", function () {
+    });
+
+    test("Cross Product", function () {
+    });
+
+    test("Length and Normalization", function () {
+    });
+
+    test("Projection", function () {
+    });
+
+});
---
+++ vector/vector.js
@@ -0,0 +1,9 @@
+/*
+ * This JavaScript file defines a Vector object and associated functions.
+ * The object itself is returned as the result of a function, allowing us
+ * to encapsulate its code and module variables.
+ */
+var Vector = (function () {
+    return {
+    };
+})();
