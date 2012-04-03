c8c9cb086d874e2e7c2f8011efee71e1bbd33be7
dondi <dondi@lmu.edu>
2012-04-02 22:48:26 -0700

    Implemented addition/subtraction suite.

--- vector/vector.js
+++ vector/vector.js
@@ -7,7 +7,15 @@ var Vector = (function () {
     // Define the constructor.
     var vector = function () {
         this.elements = [].slice.call(arguments);
-    };
+    },
+    
+        // A private method for checking dimensions,
+        // throwing an exception when different.
+        checkDimensions = function (v1, v2) {
+            if (v1.dimensions() !== v2.dimensions()) {
+                throw "Vectors have different dimensions";
+            }
+        };
 
     // Basic methods.
     vector.prototype.dimensions = function () {
@@ -28,6 +36,37 @@ var Vector = (function () {
 
     vector.prototype.w = function () {
         return this.elements[3];
+    };
+
+    // Addition and subtraction.
+    vector.prototype.add = function (v) {
+        var result = new Vector(),
+            i,
+            max;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] + v.elements[i];
+        }
+
+        return result;
+    };
+
+    vector.prototype.subtract = function (v) {
+        var result = new Vector(),
+            i,
+            max;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] - v.elements[i];
+        }
+
+        return result;
     };
 
     return vector;
