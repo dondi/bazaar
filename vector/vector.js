29b05942b09c6bb2348e956c766dc6f3b12eb257
dondi <dondi@lmu.edu>
2012-04-02 23:11:37 -0700

    Implemented product methods.

--- vector/vector.js
+++ vector/vector.js
@@ -2,6 +2,14 @@
  * This JavaScript file defines a Vector object and associated functions.
  * The object itself is returned as the result of a function, allowing us
  * to encapsulate its code and module variables.
+ *
+ * This module's approach is non-destructive: methods always return new
+ * Vector objects, and never modify the operands.  This is a design choice.
+ *
+ * This module is designed for vectors of any number of dimensions.  The
+ * implementations are generalized but not optimal for certain sizes of
+ * vectors.  Specific Vector2D and Vector3D implementations can be much
+ * more compact, while sacrificing generality.
  */
 var Vector = (function () {
     // Define the constructor.
@@ -67,6 +75,62 @@ var Vector = (function () {
         }
 
         return result;
+    };
+
+    // Scalar multiplication and division.
+    vector.prototype.multiply = function (s) {
+        var result = new Vector(),
+            i,
+            max;
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] * s;
+        }
+
+        return result;
+    };
+
+    vector.prototype.divide = function (s) {
+        var result = new Vector(),
+            i,
+            max;
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result.elements[i] = this.elements[i] / s;
+        }
+
+        return result;
+    };
+
+    // Dot product.
+    vector.prototype.dot = function (v) {
+        var result = 0,
+            i,
+            max;
+
+        // Dimensionality check.
+        checkDimensions(this, v);
+
+        for (i = 0, max = this.dimensions(); i < max; i += 1) {
+            result += this.elements[i] * v.elements[i];
+        }
+
+        return result;
+    };
+
+    // Cross product.
+    vector.prototype.cross = function (v) {
+        // This method is for 3D vectors only.
+        if (this.dimensions() !== 3 || v.dimensions() !== 3) {
+            throw "Cross product is for 3D vectors only.";
+        }
+
+        // With 3D vectors, we can just return the result directly.
+        return new Vector(
+            (this.y() * v.z()) - (this.z() * v.y()),
+            (this.z() * v.x()) - (this.x() * v.z()),
+            (this.x() * v.y()) - (this.y() * v.x())
+        );
     };
 
     return vector;
