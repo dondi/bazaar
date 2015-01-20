2bfeada5c5f85fa485aede101d7ee73aab229b09
dondi <dondi@lmu.edu>
2015-01-20 11:56:03 -0800

    Add loop to count odd arguments; no printout yet.

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,6 +1,12 @@
 public class PrintArgumentLengths {
     public static void main(String[] args) {
         System.out.println("You typed " + args.length + " arguments with that command.");
+
+        int totalOdd = 0;
+        for (String arg: args) {
+            if (arg.length() % 2 != 0) {
+                totalOdd += 1;
+            }
+        }
     }
 }
-
