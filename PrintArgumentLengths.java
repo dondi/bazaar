e6c67283d3843c301fe13abff37cb295c890abef
dondi <dondi@lmu.edu>
2015-01-20 11:51:52 -0800

    Add ability to show number of arguments.

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,4 +1,6 @@
 public class PrintArgumentLengths {
     public static void main(String[] args) {
+        System.out.println("You typed " + args.length + " arguments with that command.");
     }
 }
+
