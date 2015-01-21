9adcde6d7d0b8d188c7db80c0a6fe53823d5cb5f
dondi <dondi@lmu.edu>
2015-01-20 16:05:24 -0800

    Finish program! Odd or even is correctly displayed.

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,6 +1,6 @@
 public class PrintArgumentLengths {
     public static void main(String[] args) {
         System.out.println("You typed in " + args.length + " arguments.");
-        System.out.println("That is an (odd or even) number.");
+        System.out.println("That is an " + (args.length % 2 == 0 ? "even" : "odd") + " number.");
     }
 }
