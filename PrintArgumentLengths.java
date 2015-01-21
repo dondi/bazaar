a2963928c165ad0d5b50d5d871a82e4af2e1c315
dondi <dondi@lmu.edu>
2015-01-20 16:03:26 -0800

    Add placeholder message for odd/even.

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,5 +1,6 @@
 public class PrintArgumentLengths {
     public static void main(String[] args) {
         System.out.println("You typed in " + args.length + " arguments.");
+        System.out.println("That is an (odd or even) number.");
     }
 }
