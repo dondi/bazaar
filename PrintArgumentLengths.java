3d05735748531789dc0fa1de4b791c059b983c51
dondi <dondi@lmu.edu>
2015-02-09 18:32:15 -0800

    Remove file used for demonstration commits.

--- PrintArgumentLengths.java
+++
@@ -1,6 +0,0 @@
-public class PrintArgumentLengths {
-    public static void main(String[] args) {
-        System.out.println("You typed in " + args.length + " arguments.");
-        System.out.println("That is an " + (args.length % 2 == 0 ? "even" : "odd") + " number.");
-    }
-}
