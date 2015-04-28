49ef5a3c89115de5663faf328688363f2cf4e95f
dondi <dondi@lmu.edu>
2015-04-27 23:28:09 -0700

    Blended sample phase 1: Use two textures.

---
+++ texture-blend-webgl/texture-blend-webgl.js
@@ -0,0 +1,484 @@
+/*
+ * For maximum modularity, we place everything within a single function that
+ * takes the canvas that it will need.
+ */
+(function (canvas) {
+
+    // Because many of these variables are best initialized then immediately
+    // used in context, we merely name them here.  Read on to see how they
+    // are used.
+    var gl, // The WebGL context.
+
+        // This variable stores 3D model information.
+        objectsToDraw,
+
+        // The shader program to use.
+        shaderProgram,
+
+        // Utility variable indicating whether some fatal has occurred.
+        abort = false,
+
+        // Important state variables.  Yep, they are growing!
+        modelViewMatrix,
+        xRotationMatrix,
+        yRotationMatrix,
+        projectionMatrix,
+        vertexPosition,
+        vertexDiffuseColor,
+        vertexSpecularColor,
+        shininess,
+        rotationAroundX = 0.0,
+        rotationAroundY = 0.0,
+
+        // For emphasis, we separate the variables that involve lighting.
+        normalVector,
+        lightPosition,
+        lightDiffuse,
+        lightSpecular,
+
+        // These variables pertain to texture mapping.
+        goldTexture,
+        waterTexture,
+        textureCoordinate,
+
+        // An individual "draw object" function.
+        drawObject,
+
+        // The big "draw scene" function.
+        drawScene,
+
+        // Transient state variables for event handling.
+        xDragStart,
+        yDragStart,
+        xRotationStart,
+        yRotationStart,
+
+        // Reusable loop variables.
+        i,
+        maxi,
+        j,
+        maxj,
+
+        /*
+         * This code does not really belong here: it should live
+         * in a separate library of matrix and transformation
+         * functions.  It is here only to show you how matrices
+         * can be used with GLSL.
+         *
+         * Based on the original glRotate reference:
+         *     http://www.opengl.org/sdk/docs/man/xhtml/glRotate.xml
+         */
+        getRotationMatrix = function (angle, x, y, z) {
+            // In production code, this function should be associated
+            // with a matrix object with associated functions.
+            var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
+                s = Math.sin(angle * Math.PI / 180.0),
+                c = Math.cos(angle * Math.PI / 180.0),
+                oneMinusC = 1.0 - c,
+
+                // We can't calculate this until we have normalized
+                // the axis vector of rotation.
+                x2, // "2" for "squared."
+                y2,
+                z2,
+                xy,
+                yz,
+                xz,
+                xs,
+                ys,
+                zs;
+
+            // Normalize the axis vector of rotation.
+            x /= axisLength;
+            y /= axisLength;
+            z /= axisLength;
+
+            // *Now* we can calculate the other terms.
+            x2 = x * x;
+            y2 = y * y;
+            z2 = z * z;
+            xy = x * y;
+            yz = y * z;
+            xz = x * z;
+            xs = x * s;
+            ys = y * s;
+            zs = z * s;
+
+            // GL expects its matrices in column major order.
+            return [
+                (x2 * oneMinusC) + c,
+                (xy * oneMinusC) + zs,
+                (xz * oneMinusC) - ys,
+                0.0,
+
+                (xy * oneMinusC) - zs,
+                (y2 * oneMinusC) + c,
+                (yz * oneMinusC) + xs,
+                0.0,
+
+                (xz * oneMinusC) + ys,
+                (yz * oneMinusC) - xs,
+                (z2 * oneMinusC) + c,
+                0.0,
+
+                0.0,
+                0.0,
+                0.0,
+                1.0
+            ];
+        },
+
+        /*
+         * This is another function that really should reside in a
+         * separate library.  But, because the creation of that library
+         * is part of the student course work, we leave it here for
+         * later refactoring and adaptation by students.
+         */
+        getOrthoMatrix = function (left, right, bottom, top, zNear, zFar) {
+            var width = right - left,
+                height = top - bottom,
+                depth = zFar - zNear;
+
+            return [
+                2.0 / width,
+                0.0,
+                0.0,
+                0.0,
+
+                0.0,
+                2.0 / height,
+                0.0,
+                0.0,
+
+                0.0,
+                0.0,
+                -2.0 / depth,
+                0.0,
+
+                -(right + left) / width,
+                -(top + bottom) / height,
+                -(zFar + zNear) / depth,
+                1.0
+            ];
+        };
+
+    // Grab the WebGL rendering context.
+    gl = GLSLUtilities.getGL(canvas);
+    if (!gl) {
+        alert("No WebGL context found...sorry.");
+
+        // No WebGL, no use going on...
+        return;
+    }
+
+    // Set up settings that will not change.  This is not "canned" into a
+    // utility function because these settings really can vary from program
+    // to program.
+    gl.enable(gl.DEPTH_TEST);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    // Set up the texture object.
+    goldTexture = gl.createTexture();
+    waterTexture = gl.createTexture();
+
+    var readyTexture = 0;
+    var loadHandlerFor = function (texture, textureImage) {
+            return function () {
+                gl.bindTexture(gl.TEXTURE_2D, texture);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureImage);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+                gl.generateMipmap(gl.TEXTURE_2D);
+                gl.bindTexture(gl.TEXTURE_2D, null);
+                readyTexture += 1;
+            };
+        };
+
+    var goldImage = new Image();
+    goldImage.onload = loadHandlerFor(goldTexture, goldImage);
+    goldImage.src = "gold.jpg";
+
+    var waterImage = new Image();
+    waterImage.onload = loadHandlerFor(waterTexture, waterImage);
+    waterImage.src = "water.jpg";
+
+    // Build the objects to display. We do some mesh manipulation here to get
+    // the desired effect.
+    var goldMesh = Shapes.icosahedron();
+    goldMesh.indices = goldMesh.indices.filter(function (triangle, index) { return index % 2; });
+
+    var waterMesh = Shapes.icosahedron();
+    waterMesh.indices = waterMesh.indices.filter(function (triangle, index) { return !(index % 2); });
+
+    objectsToDraw = [
+        {
+            vertices: Shapes.toRawTriangleArray(goldMesh),
+
+            color: { r: 1.0, g: 0.0, b: 0.0 },
+            specularColor: { r: 1.0, g: 1.0, b: 1.0 },
+            shininess: 16,
+
+            // Like colors, one normal per vertex.  This can be simplified
+            // with helper functions, of course.
+            normals: Shapes.toNormalArray(goldMesh),
+
+            // One more array to associate with our vertices---texture coordinates!
+            // Here we generate them raw...some design thought may be needed in order
+            // to create/manage them in a more convenient way.
+            textureCoordinates: (function () {
+                var result = [];
+                for (var i = 0; i < 10; i += 1) {
+                    result.push(0.0, 0.0, 1.0, 0.0, 0.0, 1.0);
+                }
+                return result;
+            })(),
+
+            mode: gl.TRIANGLES
+        },
+
+        {
+            vertices: Shapes.toRawTriangleArray(waterMesh),
+
+            color: { r: 0.0, g: 0.0, b: 1.0 },
+            specularColor: { r: 1.0, g: 1.0, b: 1.0 },
+            shininess: 16,
+
+            // Like colors, one normal per vertex.  This can be simplified
+            // with helper functions, of course.
+            normals: Shapes.toNormalArray(waterMesh),
+
+            // One more array to associate with our vertices---texture coordinates!
+            // Here we generate them raw...some design thought may be needed in order
+            // to create/manage them in a more convenient way.
+            textureCoordinates: (function () {
+                var result = [];
+                for (var i = 0; i < 10; i += 1) {
+                    result.push(0.0, 0.0, 1.0, 0.0, 0.0, 1.0);
+                }
+                return result;
+            })(),
+
+            mode: gl.TRIANGLES
+        }
+    ];
+
+    // Pass the vertices to WebGL.
+    for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
+        objectsToDraw[i].buffer = GLSLUtilities.initVertexBuffer(gl,
+                objectsToDraw[i].vertices);
+
+        if (!objectsToDraw[i].colors) {
+            // If we have a single color, we expand that into an array
+            // of the same color over and over.
+            objectsToDraw[i].colors = [];
+            for (j = 0, maxj = objectsToDraw[i].vertices.length / 3;
+                    j < maxj; j += 1) {
+                objectsToDraw[i].colors = objectsToDraw[i].colors.concat(
+                    objectsToDraw[i].color.r,
+                    objectsToDraw[i].color.g,
+                    objectsToDraw[i].color.b
+                );
+            }
+        }
+        objectsToDraw[i].colorBuffer = GLSLUtilities.initVertexBuffer(gl,
+                objectsToDraw[i].colors);
+
+        // Same trick with specular colors.
+        if (!objectsToDraw[i].specularColors) {
+            // Future refactor: helper function to convert a single value or
+            // array into an array of copies of itself.
+            objectsToDraw[i].specularColors = [];
+            for (j = 0, maxj = objectsToDraw[i].vertices.length / 3;
+                    j < maxj; j += 1) {
+                objectsToDraw[i].specularColors = objectsToDraw[i].specularColors.concat(
+                    objectsToDraw[i].specularColor.r,
+                    objectsToDraw[i].specularColor.g,
+                    objectsToDraw[i].specularColor.b
+                );
+            }
+        }
+        objectsToDraw[i].specularBuffer = GLSLUtilities.initVertexBuffer(gl,
+                objectsToDraw[i].specularColors);
+
+        // One more buffer: normals.
+        objectsToDraw[i].normalBuffer = GLSLUtilities.initVertexBuffer(gl,
+                objectsToDraw[i].normals);
+ 
+        // And one more still: texture coordinates.
+        objectsToDraw[i].textureCoordinateBuffer = GLSLUtilities.initVertexBuffer(gl,
+                objectsToDraw[i].textureCoordinates);
+    }
+
+    // Initialize the shaders.
+    shaderProgram = GLSLUtilities.initSimpleShaderProgram(
+        gl,
+        $("#vertex-shader").text(),
+        $("#fragment-shader").text(),
+
+        // Very cursory error-checking here...
+        function (shader) {
+            abort = true;
+            alert("Shader problem: " + gl.getShaderInfoLog(shader));
+        },
+
+        // Another simplistic error check: we don't even access the faulty
+        // shader program.
+        function (shaderProgram) {
+            abort = true;
+            alert("Could not link shaders...sorry.");
+        }
+    );
+
+    // If the abort variable is true here, we can't continue.
+    if (abort) {
+        alert("Fatal errors encountered; we cannot continue.");
+        return;
+    }
+
+    // All done --- tell WebGL to use the shader program from now on.
+    gl.useProgram(shaderProgram);
+
+    // Hold on to the important variables within the shaders.
+    vertexPosition = gl.getAttribLocation(shaderProgram, "vertexPosition");
+    gl.enableVertexAttribArray(vertexPosition);
+    vertexDiffuseColor = gl.getAttribLocation(shaderProgram, "vertexDiffuseColor");
+    gl.enableVertexAttribArray(vertexDiffuseColor);
+    vertexSpecularColor = gl.getAttribLocation(shaderProgram, "vertexSpecularColor");
+    gl.enableVertexAttribArray(vertexSpecularColor);
+    normalVector = gl.getAttribLocation(shaderProgram, "normalVector");
+    gl.enableVertexAttribArray(normalVector);
+    textureCoordinate = gl.getAttribLocation(shaderProgram, "textureCoordinate");
+    gl.enableVertexAttribArray(textureCoordinate);
+
+    // Finally, we come to the typical setup for transformation matrices:
+    // model-view and projection, managed separately.
+    modelViewMatrix = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
+    xRotationMatrix = gl.getUniformLocation(shaderProgram, "xRotationMatrix");
+    yRotationMatrix = gl.getUniformLocation(shaderProgram, "yRotationMatrix");
+    projectionMatrix = gl.getUniformLocation(shaderProgram, "projectionMatrix");
+
+    // Note the additional variables.
+    lightPosition = gl.getUniformLocation(shaderProgram, "lightPosition");
+    lightDiffuse = gl.getUniformLocation(shaderProgram, "lightDiffuse");
+    lightSpecular = gl.getUniformLocation(shaderProgram, "lightSpecular");
+    shininess = gl.getUniformLocation(shaderProgram, "shininess");
+ 
+    /*
+     * Displays an individual object, including a transformation that now varies
+     * for each object drawn.
+     */
+    drawObject = function (object) {
+        // Set the varying colors.
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.colorBuffer);
+        gl.vertexAttribPointer(vertexDiffuseColor, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.specularBuffer);
+        gl.vertexAttribPointer(vertexSpecularColor, 3, gl.FLOAT, false, 0, 0);
+
+        // Set the shininess.
+        gl.uniform1f(shininess, object.shininess);
+
+        // Set up the model-view matrix, if an axis is included.  If not, we
+        // specify the identity matrix.
+        gl.uniformMatrix4fv(modelViewMatrix, gl.FALSE, new Float32Array(object.rotation ?
+                getRotationMatrix(object.rotation.theta,
+                        object.rotation.x, object.rotation.y, object.rotation.z) :
+                [1, 0, 0, 0, // N.B. In a full-fledged matrix library, the identity
+                 0, 1, 0, 0, //      matrix should be available as a function.
+                 0, 0, 1, 0,
+                 0, 0, 0, 1]
+            ));
+
+        // Set the varying normal vectors.
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.normalBuffer);
+        gl.vertexAttribPointer(normalVector, 3, gl.FLOAT, false, 0, 0);
+
+        // Set the texture varialbes.
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.textureCoordinateBuffer);
+        gl.vertexAttribPointer(textureCoordinate, 2, gl.FLOAT, false, 0, 0);
+
+        // Set the varying vertex coordinates.
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.buffer);
+        gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(object.mode, 0, object.vertices.length / 3);
+    };
+
+    /*
+     * Displays the scene.
+     */
+    drawScene = function () {
+        // Clear the display.
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // Set the overall rotation.
+        gl.uniformMatrix4fv(xRotationMatrix, gl.FALSE, new Float32Array(
+                getRotationMatrix(rotationAroundX, 1.0, 0.0, 0.0)
+        ));
+        gl.uniformMatrix4fv(yRotationMatrix, gl.FALSE, new Float32Array(
+                getRotationMatrix(rotationAroundY, 0.0, 1.0, 0.0)
+        ));
+
+        // Display the objects.
+        gl.activeTexture(gl.TEXTURE0);
+        for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
+            gl.bindTexture(gl.TEXTURE_2D, i % 2 ? goldTexture : waterTexture);
+            drawObject(objectsToDraw[i]);
+        }
+
+        // All done.
+        gl.flush();
+    },
+
+    /*
+     * Performs rotation calculations.
+     */
+    rotateScene = function (event) {
+        rotationAroundX = xRotationStart - yDragStart + event.clientY;
+        rotationAroundY = yRotationStart - xDragStart + event.clientX;
+        drawScene();
+    };
+
+    // Because our canvas element will not change size (in this program),
+    // we can set up the projection matrix once, and leave it at that.
+    // Note how this finally allows us to "see" a greater coordinate range.
+    // We keep the vertical range fixed, but change the horizontal range
+    // according to the aspect ratio of the canvas.  We can also expand
+    // the z range now.
+    gl.uniformMatrix4fv(projectionMatrix, gl.FALSE, new Float32Array(getOrthoMatrix(
+        -2 * (canvas.width / canvas.height),
+        2 * (canvas.width / canvas.height),
+        -2,
+        2,
+        -10,
+        10
+    )));
+
+    // Set up our one light source and its colors.
+    gl.uniform4fv(lightPosition, [-10.0, 10.0, 100.0, 1.0]);
+    gl.uniform3fv(lightDiffuse, [1.0, 1.0, 1.0]);
+    gl.uniform3fv(lightSpecular, [1.0, 1.0, 1.0]);
+
+    // Set up our texture sampler.
+    gl.uniform1i(gl.getUniformLocation(shaderProgram, "sampler"), 0);
+
+    // Instead of animation, we do interaction: let the mouse control rotation.
+    $(canvas).mousedown(function (event) {
+        xDragStart = event.clientX;
+        yDragStart = event.clientY;
+        xRotationStart = rotationAroundX;
+        yRotationStart = rotationAroundY;
+        $(canvas).mousemove(rotateScene);
+    }).mouseup(function (event) {
+        $(canvas).unbind("mousemove");
+    });
+
+    // Draw the initial scene. But we will wait until the texture is ready.
+    var drawWhenReady = setInterval(function () {
+            if (readyTexture === 2) {
+                drawScene();
+                clearInterval(drawWhenReady);
+            }
+        }, 10);
+
+}(document.getElementById("light-more-webgl")));
