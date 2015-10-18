b7bb6d410f3d344ed327980cc824e5942217daa7
dondi <dondi@lmu.edu>
2015-10-17 21:05:58 -0700

    Update calendar example to more recent jQuery version.

--- calendar-retriever/calendar-retriever.js
+++ calendar-retriever/calendar-retriever.js
@@ -5,45 +5,43 @@
 $(function () {
 
     // Make the connection.
-    $.ajax({
-        url: "http://javascript.cs.lmu.edu/php/calendar.php?date=20110606",
-        success: function (data) {
-            // We expect data to be a list of events for the
-            // date that we requested.  We will display each
-            // event in that list on the web page.
-            //
-            // If the list is empty, we show a different message.
-            if (data.length) {
-                $.each(data, function (indexInArray, valueOfElement) {
-                    // Grab the body of the page.
-                    var body = $("body");
+    $.getJSON(
+        "http://javascript.cs.lmu.edu/php/calendar.php?date=20151001"
+    ).done(function (data) {
+        // We expect data to be a list of events for the
+        // date that we requested.  We will display each
+        // event in that list on the web page.
+        //
+        // If the list is empty, we show a different message.
+        if (data.length) {
+            $.each(data, function (indexInArray, valueOfElement) {
+                // Grab the body of the page.
+                var body = $("body");
 
-                    // Add the month and year as a heading.
-                    $("<h1></h1>")
-                        .text(valueOfElement.month + " " + valueOfElement.day)
-                        .appendTo(body);
+                // Add the month and year as a heading.
+                $("<h1></h1>")
+                    .text(valueOfElement.month + " " + valueOfElement.day)
+                    .appendTo(body);
 
-                    // Add the description after the heading.
+                // Add the description after the heading.
+                $("<p></p>")
+                    .text(valueOfElement.description)
+                    .appendTo(body);
+
+                // If an event's date cannot change, indicate that.
+                if (!valueOfElement.movable) {
                     $("<p></p>")
-                        .text(valueOfElement.description)
+                        .text("This event's date cannot change from year to year.")
                         .appendTo(body);
-
-                    // If an event's date cannot change, indicate that.
-                    if (!valueOfElement.movable) {
-                        $("<p></p>")
-                            .text("This event's date cannot change from year to year.")
-                            .appendTo(body);
-                    }
-                });
-            } else {
-                $("body").append($("<p></p>")
-                    .text("No events were found for that date."));
-            }
-        },
-        error: function() {
-            $("body").append($("<p></p>"))
-                .text("The program encountered an error.");
+                }
+            });
+        } else {
+            $("body").append($("<p></p>")
+                .text("No events were found for that date."));
         }
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        $("body").append($("<p></p>")
+            .text("The program encountered an error. Most likely, this is because the returned JSON parsed badly."));
     });
 
 });
