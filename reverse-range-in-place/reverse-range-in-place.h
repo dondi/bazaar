01f8cf8110865bd1278dcf17b0467901f7347118
dondi <dondi@lmu.edu>
2016-02-14 22:37:36 -0800

    Add #ifndef guard.

--- reverse-range-in-place/reverse-range-in-place.h
+++ reverse-range-in-place/reverse-range-in-place.h
@@ -1,1 +1,6 @@
+#ifndef __RR_IN_PLACE__
+#define __RR_IN_PLACE__
+
 void reverse_range_in_place(char* string, int start_index, int end_index);
+
+#endif
--- reverse-range-in-place/test-reverse-range-in-place.h
+++ reverse-range-in-place/test-reverse-range-in-place.h
@@ -1,1 +1,6 @@
+#ifndef __TEST_RR_IN_PLACE__
+#define __TEST_RR_IN_PLACE__
+
 void test_reverse_range_in_place();
+
+#endif
