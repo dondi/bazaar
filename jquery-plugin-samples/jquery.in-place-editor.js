86db77ef627555fbadd4cc59aaf69eebd795a07d
dondi <dondi@lmu.edu>
2016-11-24 19:12:34 -0800

    Port in-place editor code to ES6.

--- jquery-plugin-samples/jquery.in-place-editor.js
+++ jquery-plugin-samples/jquery.in-place-editor.js
@@ -8,66 +8,63 @@
     change: function () { }
     - Callback for whenever an in-place editor has been invoked then dismissed.
 */
-(function ($) {
-    // Private plugin helpers.
-    var $overlay = $("<div></div>")
-            .addClass("editor-overlay");
+(($) => {
+    // Private plugin constants and helpers.
+    const INPUT_MARGIN_TOP = 3;
+    const INPUT_MARGIN_LEFT = 3;
+    const INPUT_HEIGHT_PADDING = 6;
+
+    let $overlay = $("<div></div>").addClass("editor-overlay");
 
     $.fn.inPlaceEditor = function (options) {
-        var $this = this;
-        $this.addClass("in-place-editor")
-            .hover(
-                function (event) {
-                    $(this).append($("<span></span>").addClass("in-place glyphicon glyphicon-edit"));
-                },
+        let $this = this;
+        $this.addClass("in-place-editor").hover(
+            (event) => $(event.currentTarget).append($("<span></span>").addClass("in-place glyphicon glyphicon-edit")),
+            (event) => $(event.currentTarget).find("span.in-place.glyphicon.glyphicon-edit").remove()
+        ).click((event) => {
+            let $receiver = $(event.currentTarget);
+            let receiverOffset = $receiver.offset();
 
-                function (event) {
-                    $(this).find("span.in-place.glyphicon.glyphicon-edit").remove();
-                }
-            )
-            .click(function (event) {
-                var $receiver = $(this),
-                    receiverOffset = $receiver.offset(),
-                    // Note: This plugin handles one-line entry fields only.  Support
-                    //       for textarea would be nice.
-                    $input = $("<input>")
-                        .val($receiver.text())
-                        .blur(function (event) {
-                            var $thisInput = $(this);
+            // Note: This plugin handles one-line entry fields only.  Support
+            //       for textarea would be nice.
+            let $input = $("<input>")
+                .val($receiver.text())
+                .blur((event) => {
+                    let $thisInput = $(event.currentTarget);
 
-                            // Transfer the new text over.
-                            $receiver.text($thisInput.val());
+                    // Transfer the new text over.
+                    $receiver.text($thisInput.val());
 
-                            // Clean up.
-                            $thisInput.remove();
-                            $overlay.detach();
+                    // Clean up.
+                    $thisInput.remove();
+                    $overlay.detach();
 
-                            // Invoke the callback.
-                            if ($.isFunction(options.change)) {
-                                options.change.call($receiver[0]);
-                            }
-                        })
-                        // Mimic the source as much as possible.
-                        .css({
-                            fontSize: $receiver.css('font-size'),
-                            fontStyle: $receiver.css('font-style'),
-                            fontWeight: $receiver.css('font-weight'),
-                            lineHeight: $receiver.css('line-height')
-                        })
+                    // Invoke the callback.
+                    if ($.isFunction(options.change)) {
+                        options.change.call($receiver[0]);
+                    }
+                })
+                // Mimic the source as much as possible.
+                .css({
+                    fontSize: $receiver.css('font-size'),
+                    fontStyle: $receiver.css('font-style'),
+                    fontWeight: $receiver.css('font-weight'),
+                    lineHeight: $receiver.css('line-height')
+                })
 
-                        // The tricky part---slapping an input element EXACTLY over the receiver.
-                        // Potentially fragile...
-                        .offset({
-                            top: receiverOffset.top - 3 - $(window).scrollTop(),
-                            left: receiverOffset.left - 3
-                        })
-                        .width($receiver.width())
-                        .height($receiver.height() + 6);
+                // The tricky part---slapping an input element EXACTLY over the receiver.
+                // Potentially fragile...
+                .offset({
+                    top: receiverOffset.top - INPUT_MARGIN_TOP - $(window).scrollTop(),
+                    left: receiverOffset.left - INPUT_MARGIN_LEFT
+                })
+                .width($receiver.width())
+                .height($receiver.height() + INPUT_HEIGHT_PADDING);
 
-                $("body").append($overlay);
-                $overlay.append($input);
-                $input.focus().select();
-            });
+            $("body").append($overlay);
+            $overlay.append($input);
+            $input.focus().select();
+        });
     };
 
-}(jQuery));
+})(jQuery);
