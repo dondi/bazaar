c94af534e363e5c6f4efd2e8949448e2fcbca05e
dondi <dondi@lmu.edu>
2017-01-10 23:04:29 -0800

    Pull out data as JSON and port to ES6.

--- canvas-drawing-separation/canvas-drawing-separation.js
+++ canvas-drawing-separation/canvas-drawing-separation.js
@@ -1,42 +1,18 @@
 /*
- * This program builds on the canvas drawing separation example by now
- * separating the drawing code as a function.  This lets you draw the
- * same kind of object multiple times, with varying settings.
+ * This program builds on the canvas data separation example by now
+ * separating the data as a _completely different file_ and also
+ * separating the drawing code as a function.  This lets you draw
+ * the same kind of object multiple times, with varying settings.
  */
-$(function () {
-    var canvas = $("#canvas")[0];
-    var renderingContext = canvas.getContext("2d");
+$(() => {
+    let canvas = $("#canvas")[0];
+    let renderingContext = canvas.getContext("2d");
 
-    // The curve data---two of them now, with styles.
-    var curves = [
-        {
-            startPoint: { x: 10, y: 50 },
-            controlPoint1: { x: 70, y: 10 },
-            controlPoint2: { x: 80, y: 90 },
-            endPoint: { x: 50, y: 100 },
-
-            vertexStrokeStyle: "rgba(200, 200, 200, 0.5)",
-            vertexLineWidth: 1,
-            curveStrokeStyle: "red",
-            curveLineWidth: 3
-        },
-
-        {
-            // Note the negatives.
-            startPoint: { x: -40, y: 50 },
-            controlPoint1: { x: -70, y: -30 },
-            controlPoint2: { x: 70, y: -60 },
-            endPoint: { x: 40, y: 75 },
-
-            vertexStrokeStyle: "rgb(0, 100, 100)",
-            vertexLineWidth: 1,
-            curveStrokeStyle: "blue",
-            curveLineWidth: 5
-        },
-    ];
+    // Look ma, no more curves array! That's because we've cleanly separated the
+    // curve data as a separately downloadable JSON file.
 
     // And finally, the actual curve drawing is now a function.
-    var drawCurve = function (curve) {
+    let drawCurve = (curve) => {
         // Draw the curve's vertices.
         renderingContext.strokeStyle = curve.vertexStrokeStyle;
         renderingContext.lineWidth = curve.vertexLineWidth;
@@ -60,25 +36,32 @@ $(function () {
         renderingContext.stroke();
     };
 
-    // We'll use these loop variables later.  But, to match JavaScript
-    // semantics, we declare them here.
-    var i, max;
-
-    // This is when the drawing actually happens.
-    drawCurve(curves[0]);
-    drawCurve(curves[1]);
-
-    // But the second curve is out of bounds as defined---let's use
-    // a transform to bring it in.
-    renderingContext.translate(150, 150);
-    drawCurve(curves[1]);
-
-    // With our data and code separated this cleanly, we can draw
-    // curves to our heart's content...
-    renderingContext.translate(150, 0);
-    for (i = 0, max = 5; i < max; i += 1) {
-        renderingContext.rotate(Math.PI / 12.0);
-        renderingContext.scale(1.2, 1.3);
+    // This draws the whole picture.
+    let drawPicture = (curves) => {
         drawCurve(curves[0]);
-    }
+        drawCurve(curves[1]);
+
+        // But the second curve is out of bounds as defined---let's use
+        // a transform to bring it in.
+        renderingContext.translate(150, 150);
+        drawCurve(curves[1]);
+
+        // With our data and code separated this cleanly, we can draw
+        // curves to our heart's content...
+        renderingContext.translate(150, 0);
+        for (let i = 0, max = 5; i < max; i += 1) {
+            renderingContext.rotate(Math.PI / 12.0);
+            renderingContext.scale(1.2, 1.3);
+            drawCurve(curves[0]);
+        }
+    };
+
+    // Now, we can just grab the data and draw. If we want to change the specific
+    // characteristics of the curves, we just change the JSON. This code is unaffected.
+    // Of course, if the overall _composition_ needs to be changed, then we would need
+    // to modify the `drawPicture` function.
+    //
+    // Point to ponder: Is there a way to write `drawPicture` so that it would _never_
+    // need to be rewritten?
+    $.getJSON("curves.json").then(drawPicture);
 });
