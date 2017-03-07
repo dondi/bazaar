8de71a6f5d1857ed3a3eb3fcf318ffda0a94a193
dondi <dondi@lmu.edu>
2017-03-06 21:21:17 -0800

    Transition phase 1: direct ES6 port.

--- vector/vector.js
+++ vector/vector.js
@@ -4,134 +4,134 @@
  * to encapsulate its code and module variables.
  *
  * This module's approach is non-destructive: methods always return new
- * Vector objects, and never modify the operands.  This is a design choice.
+ * Vector objects, and never modify the operands. This is a design choice.
  *
  * This module is designed for vectors of any number of dimensions.  The
  * implementations are generalized but not optimal for certain sizes of
- * vectors.  Specific Vector2D and Vector3D implementations can be much
+ * vectors. Specific Vector2D and Vector3D implementations can be much
  * more compact, while sacrificing generality.
  */
-var Vector = (function () {
-    // Define the constructor.
-    var vector = function () {
-        this.elements = [].slice.call(arguments);
-    };
-    
+window.Vector = (function () {
     // A private method for checking dimensions,
     // throwing an exception when different.
-    var checkDimensions = function (v1, v2) {
+    let checkDimensions = (v1, v2) => {
         if (v1.dimensions() !== v2.dimensions()) {
             throw "Vectors have different dimensions";
         }
     };
 
-    vector.prototype.dimensions = function () {
-        return this.elements.length;
-    };
-
-    vector.prototype.x = function () {
-        return this.elements[0];
-    };
-
-    vector.prototype.y = function () {
-        return this.elements[1];
-    };
-
-    vector.prototype.z = function () {
-        return this.elements[2];
-    };
-
-    vector.prototype.w = function () {
-        return this.elements[3];
-    };
-
-    vector.prototype.add = function (v) {
-        var result = new Vector();
-
-        checkDimensions(this, v);
-
-        for (var i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] + v.elements[i];
+    // Define the class.
+    return class Vector {
+        constructor() {
+            this.elements = [].slice.call(arguments);
         }
 
-        return result;
-    };
-
-    vector.prototype.subtract = function (v) {
-        var result = new Vector();
-
-        checkDimensions(this, v);
-
-        for (var i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] - v.elements[i];
+        dimensions() {
+            return this.elements.length;
         }
 
-        return result;
-    };
-
-    vector.prototype.multiply = function (s) {
-        var result = new Vector();
-
-        for (var i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] * s;
+        x() {
+            return this.elements[0];
         }
 
-        return result;
-    };
-
-    vector.prototype.divide = function (s) {
-        var result = new Vector();
-
-        for (var i = 0, max = this.dimensions(); i < max; i += 1) {
-            result.elements[i] = this.elements[i] / s;
+        y() {
+            return this.elements[1];
         }
 
-        return result;
-    };
-
-    vector.prototype.dot = function (v) {
-        var result = 0;
-
-        checkDimensions(this, v);
-
-        for (var i = 0, max = this.dimensions(); i < max; i += 1) {
-            result += this.elements[i] * v.elements[i];
+        z() {
+            return this.elements[2];
         }
 
-        return result;
-    };
-
-    vector.prototype.cross = function (v) {
-        if (this.dimensions() !== 3 || v.dimensions() !== 3) {
-            throw "Cross product is for 3D vectors only.";
+        w() {
+            return this.elements[3];
         }
 
-        // With 3D vectors, we can just return the result directly.
-        return new Vector(
-            (this.y() * v.z()) - (this.z() * v.y()),
-            (this.z() * v.x()) - (this.x() * v.z()),
-            (this.x() * v.y()) - (this.y() * v.x())
-        );
+        add(v) {
+            let result = new Vector();
+
+            checkDimensions(this, v);
+
+            for (let i = 0, max = this.dimensions(); i < max; i += 1) {
+                result.elements[i] = this.elements[i] + v.elements[i];
+            }
+
+            return result;
+        }
+
+        subtract(v) {
+            let result = new Vector();
+
+            checkDimensions(this, v);
+
+            for (let i = 0, max = this.dimensions(); i < max; i += 1) {
+                result.elements[i] = this.elements[i] - v.elements[i];
+            }
+
+            return result;
+        }
+
+        multiply(s) {
+            let result = new Vector();
+
+            for (let i = 0, max = this.dimensions(); i < max; i += 1) {
+                result.elements[i] = this.elements[i] * s;
+            }
+
+            return result;
+        }
+
+        divide(s) {
+            let result = new Vector();
+
+            for (let i = 0, max = this.dimensions(); i < max; i += 1) {
+                result.elements[i] = this.elements[i] / s;
+            }
+
+            return result;
+        }
+
+        dot(v) {
+            let result = 0;
+
+            checkDimensions(this, v);
+
+            for (let i = 0, max = this.dimensions(); i < max; i += 1) {
+                result += this.elements[i] * v.elements[i];
+            }
+
+            return result;
+        }
+
+        cross(v) {
+            if (this.dimensions() !== 3 || v.dimensions() !== 3) {
+                throw "Cross product is for 3D vectors only.";
+            }
+
+            // With 3D vectors, we can just return the result directly.
+            return new Vector(
+                (this.y() * v.z()) - (this.z() * v.y()),
+                (this.z() * v.x()) - (this.x() * v.z()),
+                (this.x() * v.y()) - (this.y() * v.x())
+            );
+        }
+
+        magnitude() {
+            return Math.sqrt(this.dot(this));
+        }
+
+        unit() {
+            // At this point, we can leverage our more "primitive" methods.
+            return this.divide(this.magnitude());
+        }
+
+        projection(v) {
+            checkDimensions(this, v);
+
+            // Plug and chug :)
+            // The projection of u onto v is u dot the unit vector of v
+            // times the unit vector of v.
+            let unitv = v.unit();
+            return unitv.multiply(this.dot(unitv));
+        }
     };
-
-    vector.prototype.magnitude = function () {
-        return Math.sqrt(this.dot(this));
-    };
-
-    vector.prototype.unit = function () {
-        // At this point, we can leverage our more "primitive" methods.
-        return this.divide(this.magnitude());
-    };
-
-    vector.prototype.projection = function (v) {
-        checkDimensions(this, v);
-
-        // Plug and chug :)
-        // The projection of u onto v is u dot the unit vector of v
-        // times the unit vector of v.
-        var unitv = v.unit();
-        return unitv.multiply(this.dot(unitv));
-    };
-
-    return vector;
 })();
