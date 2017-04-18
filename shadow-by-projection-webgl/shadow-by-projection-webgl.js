6320916958ccd4184567671795d5963c18ef36e3
dondi <dondi@lmu.edu>
2017-04-17 23:19:46 -0700

    One last ES6 conversion.

--- shadow-by-projection-webgl/shadow-by-projection-webgl.js
+++ shadow-by-projection-webgl/shadow-by-projection-webgl.js
@@ -2,145 +2,101 @@
  * For maximum modularity, we place everything within a single function that
  * takes the canvas that it will need.
  */
-(function (canvas) {
+((canvas) => {
+    /*
+     * This code does not really belong here: it should live
+     * in a separate library of matrix and transformation
+     * functions.  It is here only to show you how matrices
+     * can be used with GLSL.
+     *
+     * Based on the original glRotate reference:
+     *     https://www.khronos.org/registry/OpenGL-Refpages/es1.1/xhtml/glRotate.xml
+     */
+    let getRotationMatrix = (angle, x, y, z) => {
+        // In production code, this function should be associated
+        // with a matrix object with associated functions.
+        let axisLength = Math.sqrt((x * x) + (y * y) + (z * z));
+        let s = Math.sin(angle * Math.PI / 180.0);
+        let c = Math.cos(angle * Math.PI / 180.0);
+        let oneMinusC = 1.0 - c;
 
-    // Because many of these variables are best initialized then immediately
-    // used in context, we merely name them here.  Read on to see how they
-    // are used.
-    var gl, // The WebGL context.
+        // Normalize the axis vector of rotation.
+        x /= axisLength;
+        y /= axisLength;
+        z /= axisLength;
 
-        // This variable stores 3D model information.
-        objectsToDraw,
+        // Now we can calculate the other terms.
+        // "2" for "squared."
+        let x2 = x * x;
+        let y2 = y * y;
+        let z2 = z * z;
+        let xy = x * y;
+        let yz = y * z;
+        let xz = x * z;
+        let xs = x * s;
+        let ys = y * s;
+        let zs = z * s;
 
-        // The shader programs to use.
-        shaderProgram,
-        shadowProgram,
+        // GL expects its matrices in column major order.
+        return [
+            (x2 * oneMinusC) + c,
+            (xy * oneMinusC) + zs,
+            (xz * oneMinusC) - ys,
+            0.0,
 
-        // Utility variable indicating whether some fatal has occurred.
-        abort = false,
+            (xy * oneMinusC) - zs,
+            (y2 * oneMinusC) + c,
+            (yz * oneMinusC) + xs,
+            0.0,
 
-        // Important state variables.
-        animationActive = false,
-        currentRotation = 0.0,
-        currentInterval,
+            (xz * oneMinusC) + ys,
+            (yz * oneMinusC) - xs,
+            (z2 * oneMinusC) + c,
+            0.0,
 
-        // An individual "draw object" function.
-        drawObject,
+            0.0,
+            0.0,
+            0.0,
+            1.0
+        ];
+    };
 
-        // The big "draw scene" function.
-        drawScene,
+    /*
+     * This is another function that really should reside in a
+     * separate library.  But, because the creation of that library
+     * is part of the student course work, we leave it here for
+     * later refactoring and adaptation by students.
+     */
+    let getFrustumMatrix = (left, right, bottom, top, zNear, zFar) => {
+        let width = right - left;
+        let height = top - bottom;
+        let depth = zFar - zNear;
 
-        // Reusable loop variables.
-        i,
-        maxi,
-        j,
-        maxj,
+        return [
+            2.0 * zNear / width,
+            0.0,
+            0.0,
+            0.0,
 
-        /*
-         * This code does not really belong here: it should live
-         * in a separate library of matrix and transformation
-         * functions.  It is here only to show you how matrices
-         * can be used with GLSL.
-         *
-         * Based on the original glRotate reference:
-         *     http://www.opengl.org/sdk/docs/man/xhtml/glRotate.xml
-         */
-        getRotationMatrix = function (angle, x, y, z) {
-            // In production code, this function should be associated
-            // with a matrix object with associated functions.
-            var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
-                s = Math.sin(angle * Math.PI / 180.0),
-                c = Math.cos(angle * Math.PI / 180.0),
-                oneMinusC = 1.0 - c,
+            0.0,
+            2.0 * zNear / height,
+            0.0,
+            0.0,
 
-                // We can't calculate this until we have normalized
-                // the axis vector of rotation.
-                x2, // "2" for "squared."
-                y2,
-                z2,
-                xy,
-                yz,
-                xz,
-                xs,
-                ys,
-                zs;
+            (right + left) / width,
+            (top + bottom) / height,
+            -(zFar + zNear) / depth,
+            -1.0,
 
-            // Normalize the axis vector of rotation.
-            x /= axisLength;
-            y /= axisLength;
-            z /= axisLength;
-
-            // *Now* we can calculate the other terms.
-            x2 = x * x;
-            y2 = y * y;
-            z2 = z * z;
-            xy = x * y;
-            yz = y * z;
-            xz = x * z;
-            xs = x * s;
-            ys = y * s;
-            zs = z * s;
-
-            // GL expects its matrices in column major order.
-            return [
-                (x2 * oneMinusC) + c,
-                (xy * oneMinusC) + zs,
-                (xz * oneMinusC) - ys,
-                0.0,
-
-                (xy * oneMinusC) - zs,
-                (y2 * oneMinusC) + c,
-                (yz * oneMinusC) + xs,
-                0.0,
-
-                (xz * oneMinusC) + ys,
-                (yz * oneMinusC) - xs,
-                (z2 * oneMinusC) + c,
-                0.0,
-
-                0.0,
-                0.0,
-                0.0,
-                1.0
-            ];
-        },
-
-        /*
-         * This is another function that really should reside in a
-         * separate library.  But, because the creation of that library
-         * is part of the student course work, we leave it here for
-         * later refactoring and adaptation by students.
-         */
-        getFrustumMatrix = function (left, right, bottom, top, zNear, zFar) {
-            var width = right - left,
-                height = top - bottom,
-                depth = zFar - zNear;
-
-            return [
-                2.0 * zNear / width,
-                0.0,
-                0.0,
-                0.0,
-
-                0.0,
-                2.0 * zNear / height,
-                0.0,
-                0.0,
-
-                (right + left) / width,
-                (top + bottom) / height,
-                -(zFar + zNear) / depth,
-               -1.0,
-
-                0.0,
-                0.0,
-                -2 * zFar * zNear / depth,
-                0.0
-            ];
-        };
+            0.0,
+            0.0,
+            -2 * zFar * zNear / depth,
+            0.0
+        ];
+    };
 
     // Grab the WebGL rendering context.
-    gl = GLSLUtilities.getGL(canvas);
+    let gl = GLSLUtilities.getGL(canvas);
     if (!gl) {
         alert("No WebGL context found...sorry.");
 
@@ -158,7 +114,7 @@
 
     // Build the objects to display.  Note how each object may come with a
     // rotation axis now.
-    objectsToDraw = [
+    let objectsToDraw = [
         // We move our original triangles a bit to accommodate a new addition
         // to the scene (yes, a translation will also do the trick, if it
         // where implemented in this program).
@@ -295,61 +251,61 @@
     ];
 
     // Pass the vertices to WebGL.
-    for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
-        objectsToDraw[i].buffer = GLSLUtilities.initVertexBuffer(gl,
-                objectsToDraw[i].vertices);
+    objectsToDraw.forEach((objectToDraw) => {
+        objectToDraw.vertexBuffer = GLSLUtilities.initVertexBuffer(gl, objectToDraw.vertices);
 
-        if (!objectsToDraw[i].colors) {
+        if (!objectToDraw.colors) {
             // If we have a single color, we expand that into an array
             // of the same color over and over.
-            objectsToDraw[i].colors = [];
-            for (j = 0, maxj = objectsToDraw[i].vertices.length / 3;
-                    j < maxj; j += 1) {
-                objectsToDraw[i].colors = objectsToDraw[i].colors.concat(
-                    objectsToDraw[i].color.r,
-                    objectsToDraw[i].color.g,
-                    objectsToDraw[i].color.b
+            objectToDraw.colors = [];
+            for (let i = 0, maxi = objectToDraw.vertices.length / 3; i < maxi; i += 1) {
+                objectToDraw.colors = objectToDraw.colors.concat(
+                    objectToDraw.color.r,
+                    objectToDraw.color.g,
+                    objectToDraw.color.b
                 );
             }
         }
-        objectsToDraw[i].colorBuffer = GLSLUtilities.initVertexBuffer(gl,
-                objectsToDraw[i].colors);
-    }
+
+        objectToDraw.colorBuffer = GLSLUtilities.initVertexBuffer(gl, objectToDraw.colors);
+    });
 
     // Initialize the shaders.
-    shaderProgram = GLSLUtilities.initSimpleShaderProgram(
+    let abort = false;
+    let shaderProgram = GLSLUtilities.initSimpleShaderProgram(
         gl,
         $("#vertex-shader").text(),
         $("#fragment-shader").text(),
 
         // Very cursory error-checking here...
-        function (shader) {
+        (shader) => {
             abort = true;
             alert("Shader problem: " + gl.getShaderInfoLog(shader));
         },
 
         // Another simplistic error check: we don't even access the faulty
         // shader program.
-        function (shaderProgram) {
+        (shaderProgram) => {
             abort = true;
             alert("Could not link shaders...sorry.");
         }
     );
 
-    shadowProgram = GLSLUtilities.initSimpleShaderProgram(
+    // Ehrmagerd, _two_ shader programs?!?
+    let shadowProgram = GLSLUtilities.initSimpleShaderProgram(
         gl,
         $("#shadow-vertex-shader").text(),
         $("#shadow-fragment-shader").text(),
 
         // Very cursory error-checking here...
-        function (shader) {
+        (shader) => {
             abort = true;
             alert("Shader problem: " + gl.getShaderInfoLog(shader));
         },
 
         // Another simplistic error check: we don't even access the faulty
         // shader program.
-        function (shaderProgram) {
+        (shaderProgram) => {
             abort = true;
             alert("Could not link shaders...sorry.");
         }
@@ -382,7 +338,7 @@
      * Displays an individual object, including a transformation that now varies
      * for each object drawn.
      */
-    drawObject = function (object, program) {
+    let drawObject = (object, program) => {
         // Set the varying colors, if the program has them.
         if (program.vertexColor) {
             gl.bindBuffer(gl.ARRAY_BUFFER, object.colorBuffer);
@@ -391,7 +347,7 @@
 
         // Set up the model-view matrix, if an axis is included. If not, we
         // specify the identity matrix.
-        var modelViewMatrixArray = object.axis ?
+        let modelViewMatrixArray = object.axis ?
                 getRotationMatrix(currentRotation, object.axis.x, object.axis.y, object.axis.z) :
                 [1, 0, 0, 0, // N.B. In a full-fledged matrix library, the identity
                  0, 1, 0, 0, //      matrix should be available as a function.
@@ -402,7 +358,7 @@
         gl.uniformMatrix4fv(program.modelViewMatrix, gl.FALSE, new Float32Array(modelViewMatrixArray));
 
         // Set the varying vertex coordinates.
-        gl.bindBuffer(gl.ARRAY_BUFFER, object.buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.vertexBuffer);
         gl.vertexAttribPointer(program.vertexPosition, 3, gl.FLOAT, false, 0, 0);
         gl.drawArrays(object.mode, 0, object.vertices.length / 3);
     };
@@ -410,7 +366,7 @@
     /*
      * Displays the scene.
      */
-    drawScene = function () {
+    let drawScene = () => {
         // Clear the display.
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -418,17 +374,17 @@
         // the shadows are drawn last. Plus we will eliminate objects meant to be the "ground."
         gl.useProgram(shaderProgram);
         gl.disable(gl.BLEND);
-        for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
-            drawObject(objectsToDraw[i], shaderProgram);
-        }
+        objectsToDraw.forEach((objectToDraw) => {
+            drawObject(objectToDraw, shaderProgram);
+        });
 
         gl.useProgram(shadowProgram);
         gl.enable(gl.BLEND);
-        for (i = 0, maxi = objectsToDraw.length; i < maxi; i += 1) {
-            if (!objectsToDraw[i].ground) {
-                drawObject(objectsToDraw[i], shadowProgram);
-            }
-        }
+        objectsToDraw.filter((objectToDraw) => {
+            return !objectToDraw.ground;
+        }).forEach((objectToDraw) => {
+            drawObject(objectToDraw, shadowProgram);
+        });
 
         // All done.
         gl.flush();
@@ -440,23 +396,34 @@
     // We keep the vertical range fixed, but change the horizontal range
     // according to the aspect ratio of the canvas.  We can also expand
     // the z range now.
-    var projectionMatrixArray = new Float32Array(getFrustumMatrix(
-            -2 * (canvas.width / canvas.height),
-            2 * (canvas.width / canvas.height),
-            -2,
-            2,
-            12,
-            10000
-        ));
+    let projectionMatrixArray = new Float32Array(getFrustumMatrix(
+        -2 * (canvas.width / canvas.height),
+        2 * (canvas.width / canvas.height),
+        -2,
+        2,
+        12,
+        10000
+    ));
 
-    [ shaderProgram, shadowProgram ].forEach(function (program) {
+    [shaderProgram, shadowProgram].forEach((program) => {
         gl.useProgram(program);
         gl.uniformMatrix4fv(program.projectionMatrix, gl.FALSE, projectionMatrixArray);
     });
 
-    // Animation initialization/support.
-    previousTimestamp = null;
-    advanceScene = function (timestamp) {
+    /*
+     * Animates the scene.
+     */
+    let animationActive = false;
+    let currentRotation = 0.0;
+    let previousTimestamp = null;
+
+    const FRAMES_PER_SECOND = 60;
+    const MILLISECONDS_PER_FRAME = 1000 / FRAMES_PER_SECOND;
+
+    const DEGREES_PER_MILLISECOND = 0.033;
+    const FULL_CIRCLE = 360.0;
+
+    let advanceScene = (timestamp) => {
         // Check if the user has turned things off.
         if (!animationActive) {
             return;
@@ -471,17 +438,17 @@
 
         // Check if it's time to advance.
         var progress = timestamp - previousTimestamp;
-        if (progress < 30) {
+        if (progress < MILLISECONDS_PER_FRAME) {
             // Do nothing if it's too soon.
             window.requestAnimationFrame(advanceScene);
             return;
         }
 
         // All clear.
-        currentRotation += 0.033 * progress;
+        currentRotation += DEGREES_PER_MILLISECOND * progress;
         drawScene();
-        if (currentRotation >= 360.0) {
-            currentRotation -= 360.0;
+        if (currentRotation >= FULL_CIRCLE) {
+            currentRotation -= FULL_CIRCLE;
         }
 
         // Request the next frame.
@@ -493,7 +460,7 @@
     drawScene();
 
     // Set up the rotation toggle: clicking on the canvas does it.
-    $(canvas).click(function () {
+    $(canvas).click(() => {
         animationActive = !animationActive;
         if (animationActive) {
             previousTimestamp = null;
@@ -501,4 +468,4 @@
         }
     });
 
-}(document.getElementById("matrices-webgl")));
+})(document.getElementById("matrices-webgl"));
