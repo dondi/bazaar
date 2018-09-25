c7fd152a40d01224faf070186bc26682dbf22a66
dondi <dondi@lmu.edu>
2018-09-25 00:42:18 -0700

    Fix indentation.

--- giphy-sample/giphy-sample.js
+++ giphy-sample/giphy-sample.js
@@ -1,9 +1,9 @@
 /*
-   This is a very simple example of a web front end for a publicly available web service.
-   Due to its pedagogical nature, comments are more elaborate than they typically need to
-   be, or may even be present when no developer explanation would usually be necessary.
+  This is a very simple example of a web front end for a publicly available web service.
+  Due to its pedagogical nature, comments are more elaborate than they typically need to
+  be, or may even be present when no developer explanation would usually be necessary.
 
-   Further, this example uses JavaScript 2015 syntax.
+  Further, this example uses JavaScript 2015 syntax.
 */
 
 // Yes, this is a “global.” But it is a single entry point for all of the code in the module,
