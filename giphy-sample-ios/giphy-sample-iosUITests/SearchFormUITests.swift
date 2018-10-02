86a6149ac761909622d316c812779aaac55a9564
dondi <dondi@lmu.edu>
2018-10-01 18:58:20 -0700

    Give the boilerplate UI test a more specific name.

--- giphy-sample-ios/giphy-sample-iosUITests/SearchFormUITests.swift
+++ giphy-sample-ios/giphy-sample-iosUITests/SearchFormUITests.swift
@@ -1,6 +1,6 @@
 import XCTest
 
-class giphy_sample_iosUITests: XCTestCase {
+class SearchFormUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
