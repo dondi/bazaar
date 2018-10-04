a94b531e1355608ee696cb68f0c423346b9b7ebe
dondi <dondi@lmu.edu>
2018-10-04 00:05:02 -0700

    Add tests to improve coverage.

--- giphy-sample-ios/giphy-sample-iosTests/SearchResultCollectionViewControllerTests.swift
+++ giphy-sample-ios/giphy-sample-iosTests/SearchResultCollectionViewControllerTests.swift
@@ -11,6 +11,49 @@ class SearchResultCollectionViewControllerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testShouldReturnTheCorrectNumberOfSectionsAndItems() {
+        guard let searchResultCollectionViewController = UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "SearchResultCollectionViewController")
+                as? SearchResultCollectionViewController else {
+            XCTFail()
+            return
+        }
+
+        searchResultCollectionViewController.searchResultGifs =  [
+            Gif(id: "26BRBupa6nRXMGBP2", source_tld: "", images: Images(
+                fixed_width: FixedWidth(url: "https://media2.giphy.com/media/26BRBupa6nRXMGBP2/200w.gif?cid=e1bb72ff5ba9df1d5249616f457f56c5")
+            )),
+
+            Gif(id: "hklv9aNS7Gcda", source_tld: "", images: Images(
+                fixed_width: FixedWidth(url: "https://media2.giphy.com/media/hklv9aNS7Gcda/200w.gif?cid=e1bb72ff5ba9df1d5249616f457f56c5")
+            )),
+
+            Gif(id: "YJBNjrvG5Ctmo", source_tld: "", images: Images(
+                fixed_width: FixedWidth(url: "https://media0.giphy.com/media/YJBNjrvG5Ctmo/200w.gif?cid=e1bb72ff5ba9df1d5249616f457f56c5")
+            ))
+        ]
+
+        guard let collectionView = searchResultCollectionViewController.collectionView else {
+            XCTFail()
+            return
+        }
+
+        XCTAssertEqual(1, searchResultCollectionViewController.numberOfSections(in: collectionView))
+        XCTAssertEqual(3, searchResultCollectionViewController.collectionView(collectionView, numberOfItemsInSection: 0))
+    }
+
+    func testShouldAlwaysAllowItemSelection() {
+        guard let searchResultCollectionViewController = UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "SearchResultCollectionViewController")
+                as? SearchResultCollectionViewController else {
+            XCTFail()
+            return
+        }
+
+        XCTAssert(searchResultCollectionViewController.collectionView(
+            searchResultCollectionViewController.collectionView, shouldSelectItemAt: IndexPath(row: 5, section: 0)))
+    }
+
     func testShouldTriggerGiphySearchWhenSearchResultCollectionViewControllerLoads() {
         guard let searchResultCollectionViewController = UIStoryboard(name: "Main", bundle: nil)
                 .instantiateViewController(withIdentifier: "SearchResultCollectionViewController")
