c5f1269d4781dd10ddc9193781364c1efbf5b730
dondi <dondi@lmu.edu>
2018-12-02 00:24:06 -0800

    Add a log view.

--- ios-direct-manipulation-samples/ios-direct-manipulation-samples/ViewController.swift
+++ ios-direct-manipulation-samples/ios-direct-manipulation-samples/ViewController.swift
@@ -2,10 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var log: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func swivelValueChanged(_ sender: Any) {
+        if let swivel = sender as? SwivelControl {
+            log.text = "Swiveled to: \(swivel.swivelAngle)"
+        }
+    }
 }
 
