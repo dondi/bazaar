3cabea1c5b5d9d27c54fc21e6bb6460af0e14522
dondi <dondi@lmu.edu>
2019-11-19 00:56:07 -0800

    Switch to actual event dispatch in class-based Swivel test.

--- react-component-samples/src/swivel/Swivel.test.js
+++ react-component-samples/src/swivel/Swivel.test.js
@@ -1,55 +1,88 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import ReactDOM from 'react-dom'
+import ReactTestUtils from 'react-dom/test-utils'
 
 import Swivel from './Swivel'
 
 describe('Swivel direct manipulation component', () => {
-  let swivel
+  // These are declared here so that the entire test suite can get to them.
+  let div
+  let swivelComponent
 
   const transformUpdateTest = () => {
-    let tree = swivel.toJSON()
-    expect(tree).toMatchSnapshot()
+    const swivel = div.querySelector('.Swivel')
 
-    const swivelInstance = swivel.getInstance()
-    swivelInstance.handleMouseDown({ screenX: 20 })
-    swivelInstance.handleMouseMove({ screenX: 10 })
+    ReactTestUtils.act(() => {
+      ReactTestUtils.Simulate.mouseDown(swivel, { screenX: 20 })
+    })
 
-    // We check against the style attribute because the CSS property will be the generalized 'converted'
-    // value of the transform, which is too unwieldy to express manually.
-    //
-    // CSS also automatically adds the semicolons.
-    tree = swivel.toJSON()
-    expect(tree).toMatchSnapshot()
+    // We use separate act calls to let the first event “play out” fully before we invoke the next one.
+    ReactTestUtils.act(() => {
+      // The document-level event isn’t under React’s control so we invoke events on it in the same way that we would
+      // do it in a web browser.
+      document.dispatchEvent(new MouseEvent('mousemove', { screenX: 10 }))
+    })
 
-    swivelInstance.handleMouseMove({ screenX: 30 })
-    tree = swivel.toJSON()
-    expect(tree).toMatchSnapshot()
+    // Now we check the element’s rendered style.
+    expect(swivel.style.transform).toEqual('perspective(500px) rotateY(-10deg)')
 
-    swivelInstance.handleMouseUp()
-    tree = swivel.toJSON()
-    expect(tree).toMatchSnapshot()
+    ReactTestUtils.act(() => {
+      document.dispatchEvent(new MouseEvent('mousemove', { screenX: 30 }))
+    })
+
+    expect(swivel.style.transform).toEqual('perspective(500px) rotateY(10deg)')
+
+    // We do this just for completeness; for _this particular_ component, the event has no rendered effect.
+    ReactTestUtils.act(() => {
+      document.dispatchEvent(new MouseEvent('mouseup'))
+    })
   }
 
   const swivelAngleUpdateTest = () => {
-    let tree = swivel.toJSON()
-    expect(tree).toMatchSnapshot()
+    const swivel = div.querySelector('.Swivel')
 
-    const swivelInstance = swivel.getInstance()
-    swivelInstance.handleMouseDown({ screenX: 20 })
-    swivelInstance.handleMouseMove({ screenX: 10 })
-    expect(swivelInstance.state.swivelAngle).toBe(-10)
+    ReactTestUtils.act(() => {
+      ReactTestUtils.Simulate.mouseDown(swivel, { screenX: 20 })
+    })
 
-    swivelInstance.handleMouseMove({ screenX: 30 })
-    expect(swivelInstance.state.swivelAngle).toBe(10)
+    ReactTestUtils.act(() => {
+      document.dispatchEvent(new MouseEvent('mousemove', { screenX: 10 }))
+    })
 
-    swivelInstance.handleMouseUp()
+    // Instead of the style, this time we check the component’s internal state value.
+    expect(swivelComponent.state.swivelAngle).toBe(-10)
+
+    ReactTestUtils.act(() => {
+      document.dispatchEvent(new MouseEvent('mousemove', { screenX: 30 }))
+    })
+
+    expect(swivelComponent.state.swivelAngle).toBe(10)
+
+    ReactTestUtils.act(() => {
+      document.dispatchEvent(new MouseEvent('mouseup'))
+    })
   }
 
   describe('installed behavior with callback', () => {
     const mockOnChange = jest.fn()
 
     beforeEach(() => {
-      swivel = renderer.create(<Swivel label="Hello swivel" onChange={mockOnChange} />)
+      div = document.createElement('div')
+
+      // The `ref` property allows us to hold on to the React component, which we will need when inspecting
+      // its state (doable on class-based components only).
+      ReactDOM.render(
+        <Swivel
+          label="Hello swivel"
+          onChange={mockOnChange}
+          ref={component => swivelComponent = component}
+        />,
+        div
+      )
+    })
+
+    afterEach(() => {
+      ReactDOM.unmountComponentAtNode(div)
     })
 
     it('should update its CSS transform correctly', transformUpdateTest)
@@ -58,21 +91,46 @@ describe('Swivel direct manipulation component', () => {
     it('should invoke the callback correctly', () => {
       mockOnChange.mockReset()
 
-      const swivelInstance = swivel.getInstance()
-      swivelInstance.handleMouseDown({ screenX: 20 })
-      swivelInstance.handleMouseMove({ screenX: 10 })
+      const swivel = div.querySelector('.Swivel')
+
+      ReactTestUtils.act(() => {
+        ReactTestUtils.Simulate.mouseDown(swivel, { screenX: 20 })
+      })
+
+      ReactTestUtils.act(() => {
+        document.dispatchEvent(new MouseEvent('mousemove', { screenX: 10 }))
+      })
+
+      // This time, we check whether our callback got invoked correctly.
       expect(mockOnChange.mock.calls[0]).toEqual([0, -10])
 
-      swivelInstance.handleMouseMove({ screenX: 30 })
+      ReactTestUtils.act(() => {
+        document.dispatchEvent(new MouseEvent('mousemove', { screenX: 30 }))
+      })
+
       expect(mockOnChange.mock.calls[1]).toEqual([-10, 10])
 
-      swivelInstance.handleMouseUp()
+      // We do this just for completeness; for _this particular_ component, the event has no rendered effect.
+      ReactTestUtils.act(() => {
+        document.dispatchEvent(new MouseEvent('mouseup'))
+      })
     })
   })
 
   describe('installed behavior without callback', () => {
     beforeEach(() => {
-      swivel = renderer.create(<Swivel label="Hello swivel" />)
+      div = document.createElement('div')
+      ReactDOM.render(
+        <Swivel
+          label="Hello swivel"
+          ref={component => swivelComponent = component}
+        />,
+        div
+      )
+    })
+
+    afterEach(() => {
+      ReactDOM.unmountComponentAtNode(div)
     })
 
     it('should update its CSS transform correctly', transformUpdateTest)
